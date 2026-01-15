de299d926effa8989d8cdcbc283e0dddde90b9d4
tahmid <mstahmid0@gmail.com>
2026-01-14 18:05:57 -0600

    added model Link

--- mylinks/core/models.py
+++ mylinks/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Link(models.Model):
+    # title, url, icon_name
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    icon_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
